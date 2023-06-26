3beed4a439f9ff39ab35447de0c77d38e949a6bf
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-06-26 13:54:20 +0600

    Update References

--- src/components/references/References.jsx
+++ src/components/references/References.jsx
@@ -7,24 +7,45 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 
+// import required modules
+import { Pagination } from "swiper";
+
 const References = () => {
   return (
     <section className='references container section' id='references'>
       <h2 className='section__title'>References</h2>
       <span className='section__subtitle'> Professionals who can vouch for my skills and work history</span>
 
-      <div className='references__container'>
+          <Swiper className='references__container'
+            loop={true}
+            grabCursor={true}
+            spaceBetween={24}
+              
+            pagination={{
+                clickable: true,
+            }}
+              
+            breakpoints={{
+                576: {
+                slidesPerView: 2,
+                },
+                768: {
+                slidesPerView: 2,
+                spaceBetween: 48,
+                },
+            }}
+          modules={[Pagination]}>
               {Data.map(({ id, title, designation, institution, email }) => {
                   return (
-                    <div className='reference_card' key={id}>
-                          <h3 className='reference__name'>{title}</h3>
+                    <SwiperSlide className='reference__card' key={id}>
+                          <h2 className='reference__name'>{title}</h2>
                           <h4 className='reference__desig'>{designation}</h4>
                           <h4 className='reference__insti'>{institution}</h4>
-                          <h4 className='reference__email'>{email}</h4>
-                    </div>
+                          <h4 className='reference__email'>Email: {email}</h4>
+                    </SwiperSlide>
             )
         })}
-      </div>
+      </Swiper>
     </section>
   )
 }
