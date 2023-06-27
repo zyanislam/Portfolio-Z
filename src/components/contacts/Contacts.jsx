d268a715a07ce49fb9937663bb59eb7bb078d040
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-06-27 17:53:20 +0600

    Update Contacts.jsx

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -18,16 +18,7 @@ const Contacts = () => {
                 <h3 className='contact__card-title'>Email</h3>
                 <span className='contact__card-data'>sheikh.zyanislam@gmail.com</span>
               
-              <a href="mailto:sheikh.zyanislam@gmail.com.com" className='contact__button'>Write me {""}<i className='bx bx-right-arrow-alt contact__button-icon'></i> </a>
-              </div>
-
-              <div className='contact__card'>
-                <i className='bx bx contact__card-icon'></i>
-              
-                <h3 className='contact__card-title'>WhatsApp</h3>
-                <span className='contact__card-data'>8801616696562</span>
-              
-              <a href="" className='contact__button'>Write me {""}<i className='bx bx-right-arrow-alt contact__button-icon'></i> </a>
+              <a href="mailto:sheikh.zyanislam@gmail.com.com" className='contact__button'>Write me {" "}<i className='bx bx-right-arrow-alt contact__button-icon'></i> </a>
               </div>
 
               <div className='contact__card'>
@@ -44,10 +35,6 @@ const Contacts = () => {
 
           <div className='contact__content'>
             <h3 className='contact__title'>Write me your project</h3>
-          </div>
-        
-          <div className='contact__content'>
-            <h3 className='contact__title'></h3>
           </div>
           
         </div>
