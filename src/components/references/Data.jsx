a45e2db0a9bd81f7826c4ff994febb85b2045fd5
Zyan <fislam183070@bscse.uiu.ac.bd>
2024-04-05 20:19:55 +0600

    Update References

--- src/components/references/Data.jsx
+++ src/components/references/Data.jsx
@@ -1,30 +1,30 @@
 export const Data = [
-    {
-        id: 1,
-        title: "Swakkhar Shatabda",
-        designation: "Professor & Director of Data Science Department",
-        institution: "United International University",
-        email: "swakkhar@cse.uiu.ac.bd",
-    },
-    {
-        id: 2,
-        title: "Suman Ahmmed",
-        designation: "Asst. Professor & Director of Center for Development of IT Professionals (CDIP)",
-        institution: "United International University",
-        email: "suman@cse.uiu.ac.bd",
-    },
-    {
-        id: 3,
-        title: "Md. Hasan Al Kayem",
-        designation: "Lecturer",
-        institution: "United International University",
-        email: "hasan@cse.uiu.ac.bd",
-    },
-    {
-        id: 4,
-        title: "Parvez Hossain",
-        designation: "Software Test Engineer",
-        institution: "Robi Axiata Ltd.",
-        email: "parvez9605@gmail.com",
-    },
-]
+  {
+    id: 1,
+    title: "Swakkhar Shatabda",
+    designation: "Professor & Director of Data Science Department",
+    institution: "United International University",
+    email: "swakkhar@cse.uiu.ac.bd",
+  },
+  {
+    id: 2,
+    title: "Dr. Suman Ahmmed",
+    designation: "Asst. Professor",
+    institution: "United International University",
+    email: "suman@cse.uiu.ac.bd",
+  },
+  {
+    id: 3,
+    title: "Md. Hasan Al Kayem",
+    designation: "Lecturer",
+    institution: "United International University",
+    email: "hasan@cse.uiu.ac.bd",
+  },
+  {
+    id: 4,
+    title: "Emdad Ullah",
+    designation: "Principal Software Engineer",
+    institution: "RedDot Digital Limited (Robi Axiata)",
+    email: "emdad.ullah@reddotdigitalit.com",
+  },
+];
