28ed9e1cbe080481b2e5516277154b7adec240ac
Zyan <fislam183070@bscse.uiu.ac.bd>
2024-04-07 15:29:06 +0600

    Update Info

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -6,18 +6,20 @@ const Info = () => {
       <div className="about__box">
         <i class="uil uil-graduation-cap about__icon"></i>
         <h3 className="about__title">Education</h3>
-        <span className="about__subtitle">BSc. in CSE</span>
+        <span className="about__subtitle">BSc. in CSE</span> <br />
+        <span className="about__subtitle">CGPA 3.75 / 4.00</span>
       </div>
 
       <div className="about__box">
         <i class="uil uil-layers about__icon"></i>
-        <h3 className="about__title">Experience</h3>
+        <h3 className="about__title">Work Experience</h3>
+        <span className="about__subtitle">Software Eng.</span> <br />
         <span className="about__subtitle">7 months</span>
       </div>
 
       <div className="about__box">
-        <i class="bx bxs-bolt-circle about__icon"></i>
-        <h3 className="about__title">Completed</h3>
+        <i class="bx bxs-bolt-circle about__icon2"></i>
+        <h3 className="about__title">Projects Completed</h3>
         <span className="about__subtitle">15+ Projects</span>
       </div>
     </div>
